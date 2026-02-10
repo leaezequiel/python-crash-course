36205cf523d50e9e5e71828e71730f06e142a826
lean <lea.ezequiel@live.com>
2026-02-10 15:37:58 -0300

    Se agrega carpeta y primeros ejercicios u10

---
+++ seccion_1_basics/10_archivos_y_excepciones/10-3.codigo_mas_sencillo.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 10 09:44:08 2026
+
+@author: lstivanello
+"""
+
+"""
+10-3. Simpler Code: The program file_reader.py in this section uses a temporary variable, lines, to show how splitlines() works. You can skip the temporary variable and loop directly over the list that splitlines() returns:
+for line in contents.splitlines():
+Remove the temporary variable from each of the programs in this section, to make them more concise.
+"""
+
+print('Ya lo habia hecho asi. genero este archivo para mantener estructura nomas.')
